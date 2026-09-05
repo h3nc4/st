9e4729b54ae5af0d12b8424dbb91a707c0658064
h3nc4 <me@h3nc4.com>
2026-09-04 23:12:58 -0300

    run ci in the shared dev container image

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Terminess Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Terminess Nerd Font:pixelsize=24:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = { 
 	// "Terminess Nerd Font Mono:pixelsize=14:antialias=true:autohint=true",
